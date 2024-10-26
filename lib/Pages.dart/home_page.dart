497d169eb6375d8e10a739b55522da1d27adb252
ZaynShelby <z4zayn01@gmail.com>
2024-10-26 19:21:41 +0530

    first commit

--- lib/Pages.dart/home_page.dart
+++ lib/Pages.dart/home_page.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Center(
+      body: const Center(
         child: Text("I'm Coming Soon")),
       appBar: AppBar(
         title: const Center(
